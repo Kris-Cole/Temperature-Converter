5826ca95bca5cc2d6f20f3ea851f4f8896f0576f
Kris Cole <thekriscole3000@gmail.com>
2025-01-13 01:17:52 +0100

    Added kelvin input and theme toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const celsiusInput = document.getElementById("celsius");
     const fahrenheitInput = document.getElementById("fahrenheit");
+    const kelvinInput = document.getElementById("kelvin");
     const resetButton = document.getElementById("reset");
+    const themeToggle = document.getElementById("theme-toggle");
+    const errorMessage = document.getElementById("error-message");
 
     // Convert Celsius to Fahrenheit
     celsiusInput.addEventListener("input", () => {
         const celsius = parseFloat(celsiusInput.value);
         if (!isNaN(celsius)) {
             fahrenheitInput.value = ((celsius * 9) / 5 + 32).toFixed(2);
+            kelvinInput.value = (celsius + 273.15).toFixed(2);
         } else {
             fahrenheitInput.value = "";
+            kelvinInput.value = "";
         }
     });
 
@@ -18,14 +23,41 @@ document.addEventListener("DOMContentLoaded", () => {
         const fahrenheit = parseFloat(fahrenheitInput.value);
         if (!isNaN(fahrenheit)) {
             celsiusInput.value = ((fahrenheit - 32) * 5 / 9).toFixed(2);
+            kelvinInput.value = (celsius + 273.15).toFixed(2);
         } else {
             celsiusInput.value = "";
+            kelvinInput.value = "";
         }
     });
+
+    // Conver Kelvin to Celsius and Fahrenheit
+    kelvinInput.addEventListener("input", () => {
+        const kelvin = parseFloat(kelvinInput.value);
+        if (!isNaN(kelvin) && kelvin >= 0) {
+            celsiusInput.value = (kelvin + 273.15).toFixed(2);
+            fahrenheitInput.value = ((celsius * 9) / 5 + 32).toFixed(2);
+        } else if (kelvin < 0) {
+            errorMessage.textContent = "Kelvin cannot be negative!";
+            errorMessage.classList.remove("hidden");
+            celsiusInput.value = "";
+            fahrenheitInput.value = "";
+        } else {
+            errorMessage.classList.add("hidden");
+            celsiusInput.value = "";
+            fahrenheitInput.value = "";
+        }
+    })
 
     // Reset both input fields
     resetButton.addEventListener('click', () => {
         celsiusInput.value = "";
         fahrenheitInput.value = "";
+        kelvinInput.value = "";
+        errorMessage.classList.add("hidden");
+    });
+
+    // Toggle Theme
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-theme");
     });
 });
